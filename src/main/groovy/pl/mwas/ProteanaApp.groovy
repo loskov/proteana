7a4f4a82ed7eb5411ec5d2ab7811d8e6f4fedc79
Mateusz Was <mateusz.was@tuatara.pl>
2016-04-25 14:11:42 +0200

    fix annotation to @SpringBootApplication

--- src/main/groovy/pl/mwas/ProteanaApp.groovy
+++ src/main/groovy/pl/mwas/ProteanaApp.groovy
@@ -1,15 +1,11 @@
 package pl.mwas
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ProteanaApp {
     static void main(String[] args) throws Exception {
-        SpringApplication.run(ProteanaApp.class, args)
+        SpringApplication.run(ProteanaApp, args)
     }
 }
